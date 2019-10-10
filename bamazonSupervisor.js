7bb8f5c3d6aacbcd064a0f4fa91c65bb8096f8fc
codeproj01 <cliftonmpls@comcast.net>
2019-10-09 23:37:03 -0500

    Deploying Node.js & MySQL

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -61,18 +61,17 @@ function createNewDept() {
 };
 
 function supervisorsViewOfSales() {
-    //Implement a table join
-    //connection.query("SELECT department_id, d.department_name, over_head_costs, sum(product_sales) AS product_sales, (sum(product_sales) - over_head_costs) AS total_profit FROM departments d INNER JOIN products p ON d.department_name = p.department_name GROUP BY department_name", function(err, res){
+    
     connection.query("SELECT B.department_id, A.department_name, b.over_head_costs, SUM(A.product_sales) AS Total_Sales_By_Dept, SUM(A.product_sales) - B.over_head_costs AS Profit FROM products A, departments B WHERE a.department_name = b.department_name GROUP BY department_name, department_id ORDER BY department_id", 
         function(err, res) {
     
         if (err) throw err;
-        //Construct cli-table headers
+        
         let table = new Table({
            head: ["department_id", "department_name", "over_head_costs", "products_sales", "total_profits"]
             //head: ["department_id", "department_name", "over_head_costs", "total_sales_by_Dept", "profit"]
         });
-        //Display content in cli-table
+        
         for (let i = 0; i < res.length; i++) {
             table.push([res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].product_sales, res[i].total_profit])
         }
